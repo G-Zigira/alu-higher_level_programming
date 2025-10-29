f174380f772d6c4ef75f0c729a958dbe33761fe1
G-Zigira <g.zigira@alustudent.com>
2025-10-29 09:00:01 -0700

    3rd atp

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#ptyhon script that prints the ASCII alphabet followed by new line
 for i in range(97, 123):
     if i != 101 and i != 113:
-        print("{}".format(chr(i)), end="")
+    print("{}".format(chr(i)), end="")
