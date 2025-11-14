639de4cf2f26d39f744470f2ac25c3b219a8c041
G-Zigira <g.zigira@alustudent.com>
2025-11-14 11:30:18 -0800

    1st

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-"""
-defining the square class
-"""
-
+"""defining the square class """
 
 class Square:
-    """
-    represents a square with a given size 
-    """
-
+    """    represents a square with a given size """
+    
     def __init__(self, size):
-        """
-        initializes the square object with a specific size from either assignment from the user or the system
-        """
+        """initializes the square object with a specific size from either assignment from the source code or the users"""
         self.__size = size
