6ae1c6419cbc9f0c095014fd91b3a90d4620c51e
G-Zigira <g.zigira@alustudent.com>
2025-11-29 19:25:54 +0200

    hei

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a url and displays its response body size
-curl -sI "$1" | grep "Content-Length" | awk '{print $2}'
+# display Content-Length
+curl -sI "$1" | grep 'Content-Length' | cut -d " " -f2
